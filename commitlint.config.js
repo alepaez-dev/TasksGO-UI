b816eaba27452e94e954d69f9ee84fe5bec95be5
Ale Paez <alepaezdev@gmail.com>
2026-03-09 16:46:04 -0700

    hotfix: regenerate lock file with cross-platform optiona deps

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,20 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [2, 'always', 200],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'refactor',
+        'test',
+        'ci',
+        'revert',
+        'hotfix',
+      ],
+    ],
   },
 };
