1b77c15b30faa346acc054dfb9d6f56db992bac8
alepaez-dev <alepaezdev@gmail.com>
2026-05-30 20:06:01 +0000

    Deploying to gh-pages from @ alepaez-dev/TasksGO-UI@7ccfcfa35037d8227e09a701a8bd7e0733b8b768 🚀

---
+++ assets/index-Bd5QeUsb.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-M7vZPr2E.js";import{r as o}from"./index-BLPlLp0A.js";var t=o();const m=r(t);export{m as R,t as r};
--- assets/index-DDRe7C_U.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-BWQ4AAfR.js";import{r as o}from"./index-CXMzldcN.js";var t=o();const m=r(t);export{m as R,t as r};
