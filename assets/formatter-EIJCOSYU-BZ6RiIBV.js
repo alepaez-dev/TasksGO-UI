d58aafb34bffbb702d041c0685072077e24d4e2b
alepaez-dev <alepaezdev@gmail.com>
2026-05-30 21:05:16 +0000

    Deploying to gh-pages from @ alepaez-dev/TasksGO-UI@9744fb057444f64830422157836fba1c10710be9 🚀

--- assets/formatter-EIJCOSYU-BZ6RiIBV.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-LL677BLK-B_wvMJ5F.js";import{b as t}from"./iframe-M7vZPr2E.js";import"./preload-helper-CyvMmb0J.js";import"./client-liVxIje5.js";import"./index-BLPlLp0A.js";import"./index-Bd5QeUsb.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
