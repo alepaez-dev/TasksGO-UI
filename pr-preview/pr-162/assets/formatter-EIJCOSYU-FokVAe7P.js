44c7509b6aa660fed1cd7356805c85070e9f91a6
alepaez-dev <alepaez-dev@users.noreply.github.com>
2026-07-05 01:09:40 +0000

    Deploy preview for PR 162 🛫

---
+++ pr-preview/pr-162/assets/formatter-EIJCOSYU-FokVAe7P.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-UypE2Twa.js";import{b as t}from"./iframe-BzwXrY0i.js";import"./preload-helper-CtMkiQxc.js";import"./client-RuXB3PIW.js";import"./index-DoUUa4mW.js";import"./index-Wk9tjzH2.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
