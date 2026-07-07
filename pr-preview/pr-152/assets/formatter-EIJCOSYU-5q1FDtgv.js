0909d155e89b9745ecb6e1e191e8b7153fc67faa
alepaez-dev <alepaez-dev@users.noreply.github.com>
2026-07-07 23:16:48 +0000

    Deploy preview for PR 152 🛫

---
+++ pr-preview/pr-152/assets/formatter-EIJCOSYU-5q1FDtgv.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-BRaYekTj.js";import{b as t}from"./iframe-BHPjif0Y.js";import"./preload-helper-C_1rgeVj.js";import"./client-Dm0LS9KJ.js";import"./index-CZvBnvO4.js";import"./index-C1dAJB6M.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
