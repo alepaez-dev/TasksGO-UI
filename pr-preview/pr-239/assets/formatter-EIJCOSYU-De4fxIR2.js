d3872041bd57bfb388070bc94d2b29ef20e87cbe
alepaez-dev <alepaez-dev@users.noreply.github.com>
2026-08-23 13:33:17 +0000

    Deploy preview for PR 239 🛫

---
+++ pr-preview/pr-239/assets/formatter-EIJCOSYU-De4fxIR2.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-BAwU0X-E.js";import{b as t}from"./iframe-BhzNsdOX.js";import"./preload-helper-BbkmO2H3.js";import"./client-36CDWJHN.js";import"./index-Dr7BULin.js";import"./index-B0SZ4GPW.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
