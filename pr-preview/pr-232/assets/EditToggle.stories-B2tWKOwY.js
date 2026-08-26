353a6ac8e840f45d7fef6db835ee2cc862877b3e
alepaez-dev <alepaez-dev@users.noreply.github.com>
2026-08-26 23:26:05 +0000

    Remove preview for PR 232 🛬

--- pr-preview/pr-232/assets/EditToggle.stories-B2tWKOwY.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-u17CrQMm.js";import{r as a}from"./iframe-Bmmcrpch.js";import{E as o}from"./EditToggle-BXhMCScq.js";import"./preload-helper-CZDhsGh7.js";import"./cn-2dOUpm6k.js";import"./Icon-D4TrrGt1.js";const u={title:"Components/EditToggle",component:o,parameters:{layout:"centered"}};function i(){const[n,s]=a.useState(!1);return t.jsx(o,{editing:n,onEditingChange:s})}const e={render:()=>t.jsx(i,{})},r={render:()=>t.jsx(o,{editing:!0,onEditingChange:()=>{}})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: () => <Controlled />
-}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => <EditToggle editing onEditingChange={() => {}} />
-}`,...r.parameters?.docs?.source}}};const E=["Default","Editing"];export{e as Default,r as Editing,E as __namedExportsOrder,u as default};
--- pr-preview/pr-232/assets/index-6bMRshan.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-Bmmcrpch.js";import{r as o}from"./index-BDWVZZ6h.js";var t=o();const m=r(t);export{m as R,t as r};
